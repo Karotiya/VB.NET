d4861ddf9803244cd53691483bbe837e1fb44830
Karotiya <145431636+Karotiya@users.noreply.github.com>
2023-09-19 12:54:24 +0530

    Add files via upload

---
+++ Notepad app/Form1.Designer.vb
@@ -0,0 +1,293 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        MenuStrip1 = New MenuStrip()
+        FileToolStripMenuItem = New ToolStripMenuItem()
+        NewToolStripMenuItem = New ToolStripMenuItem()
+        OpenToolStripMenuItem = New ToolStripMenuItem()
+        SaveToolStripMenuItem = New ToolStripMenuItem()
+        ExitToolStripMenuItem = New ToolStripMenuItem()
+        EditToolStripMenuItem = New ToolStripMenuItem()
+        CutToolStripMenuItem = New ToolStripMenuItem()
+        CopyToolStripMenuItem = New ToolStripMenuItem()
+        PasteToolStripMenuItem = New ToolStripMenuItem()
+        ReduToolStripMenuItem = New ToolStripMenuItem()
+        UndoToolStripMenuItem = New ToolStripMenuItem()
+        FormatToolStripMenuItem = New ToolStripMenuItem()
+        FontToolStripMenuItem = New ToolStripMenuItem()
+        ColorToolStripMenuItem = New ToolStripMenuItem()
+        AlignToolStripMenuItem = New ToolStripMenuItem()
+        LeftToolStripMenuItem = New ToolStripMenuItem()
+        RightToolStripMenuItem = New ToolStripMenuItem()
+        CenterToolStripMenuItem = New ToolStripMenuItem()
+        BulleteToolStripMenuItem = New ToolStripMenuItem()
+        YeToolStripMenuItem = New ToolStripMenuItem()
+        NOToolStripMenuItem = New ToolStripMenuItem()
+        ZoomToolStripMenuItem = New ToolStripMenuItem()
+        ToolStripMenuItem2 = New ToolStripMenuItem()
+        ToolStripMenuItem3 = New ToolStripMenuItem()
+        ToolStripMenuItem4 = New ToolStripMenuItem()
+        ToolStripMenuItem5 = New ToolStripMenuItem()
+        RichTextBox1 = New RichTextBox()
+        SaveFileDialog1 = New SaveFileDialog()
+        OpenFileDialog1 = New OpenFileDialog()
+        FontDialog1 = New FontDialog()
+        ColorDialog1 = New ColorDialog()
+        MenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.ImageScalingSize = New Size(22, 22)
+        MenuStrip1.Items.AddRange(New ToolStripItem() {FileToolStripMenuItem, EditToolStripMenuItem, FormatToolStripMenuItem, ZoomToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(800, 30)
+        MenuStrip1.TabIndex = 0
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' FileToolStripMenuItem
+        ' 
+        FileToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {NewToolStripMenuItem, OpenToolStripMenuItem, SaveToolStripMenuItem, ExitToolStripMenuItem})
+        FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        FileToolStripMenuItem.Size = New Size(48, 26)
+        FileToolStripMenuItem.Text = "File"
+        ' 
+        ' NewToolStripMenuItem
+        ' 
+        NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        NewToolStripMenuItem.Size = New Size(243, 30)
+        NewToolStripMenuItem.Text = "New"
+        ' 
+        ' OpenToolStripMenuItem
+        ' 
+        OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        OpenToolStripMenuItem.Size = New Size(243, 30)
+        OpenToolStripMenuItem.Text = "Open"
+        ' 
+        ' SaveToolStripMenuItem
+        ' 
+        SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        SaveToolStripMenuItem.Size = New Size(243, 30)
+        SaveToolStripMenuItem.Text = "Save"
+        ' 
+        ' ExitToolStripMenuItem
+        ' 
+        ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        ExitToolStripMenuItem.Size = New Size(243, 30)
+        ExitToolStripMenuItem.Text = "Exit"
+        ' 
+        ' EditToolStripMenuItem
+        ' 
+        EditToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {CutToolStripMenuItem, CopyToolStripMenuItem, PasteToolStripMenuItem, ReduToolStripMenuItem, UndoToolStripMenuItem})
+        EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        EditToolStripMenuItem.Size = New Size(50, 26)
+        EditToolStripMenuItem.Text = "Edit"
+        ' 
+        ' CutToolStripMenuItem
+        ' 
+        CutToolStripMenuItem.Name = "CutToolStripMenuItem"
+        CutToolStripMenuItem.Size = New Size(243, 30)
+        CutToolStripMenuItem.Text = "Cut"
+        ' 
+        ' CopyToolStripMenuItem
+        ' 
+        CopyToolStripMenuItem.Name = "CopyToolStripMenuItem"
+        CopyToolStripMenuItem.Size = New Size(243, 30)
+        CopyToolStripMenuItem.Text = "Copy"
+        ' 
+        ' PasteToolStripMenuItem
+        ' 
+        PasteToolStripMenuItem.Name = "PasteToolStripMenuItem"
+        PasteToolStripMenuItem.Size = New Size(243, 30)
+        PasteToolStripMenuItem.Text = "Paste"
+        ' 
+        ' ReduToolStripMenuItem
+        ' 
+        ReduToolStripMenuItem.Name = "ReduToolStripMenuItem"
+        ReduToolStripMenuItem.Size = New Size(243, 30)
+        ReduToolStripMenuItem.Text = "Redo"
+        ' 
+        ' UndoToolStripMenuItem
+        ' 
+        UndoToolStripMenuItem.Name = "UndoToolStripMenuItem"
+        UndoToolStripMenuItem.Size = New Size(243, 30)
+        UndoToolStripMenuItem.Text = "Undo"
+        ' 
+        ' FormatToolStripMenuItem
+        ' 
+        FormatToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {FontToolStripMenuItem, ColorToolStripMenuItem, AlignToolStripMenuItem, BulleteToolStripMenuItem})
+        FormatToolStripMenuItem.Name = "FormatToolStripMenuItem"
+        FormatToolStripMenuItem.Size = New Size(74, 26)
+        FormatToolStripMenuItem.Text = "Format"
+        ' 
+        ' FontToolStripMenuItem
+        ' 
+        FontToolStripMenuItem.Name = "FontToolStripMenuItem"
+        FontToolStripMenuItem.Size = New Size(243, 30)
+        FontToolStripMenuItem.Text = "Font"
+        ' 
+        ' ColorToolStripMenuItem
+        ' 
+        ColorToolStripMenuItem.Name = "ColorToolStripMenuItem"
+        ColorToolStripMenuItem.Size = New Size(243, 30)
+        ColorToolStripMenuItem.Text = "Color"
+        ' 
+        ' AlignToolStripMenuItem
+        ' 
+        AlignToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {LeftToolStripMenuItem, RightToolStripMenuItem, CenterToolStripMenuItem})
+        AlignToolStripMenuItem.Name = "AlignToolStripMenuItem"
+        AlignToolStripMenuItem.Size = New Size(243, 30)
+        AlignToolStripMenuItem.Text = "Align"
+        ' 
+        ' LeftToolStripMenuItem
+        ' 
+        LeftToolStripMenuItem.Name = "LeftToolStripMenuItem"
+        LeftToolStripMenuItem.Size = New Size(147, 30)
+        LeftToolStripMenuItem.Text = "Left"
+        ' 
+        ' RightToolStripMenuItem
+        ' 
+        RightToolStripMenuItem.Name = "RightToolStripMenuItem"
+        RightToolStripMenuItem.Size = New Size(147, 30)
+        RightToolStripMenuItem.Text = "Right"
+        ' 
+        ' CenterToolStripMenuItem
+        ' 
+        CenterToolStripMenuItem.Name = "CenterToolStripMenuItem"
+        CenterToolStripMenuItem.Size = New Size(147, 30)
+        CenterToolStripMenuItem.Text = "Center"
+        ' 
+        ' BulleteToolStripMenuItem
+        ' 
+        BulleteToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {YeToolStripMenuItem, NOToolStripMenuItem})
+        BulleteToolStripMenuItem.Name = "BulleteToolStripMenuItem"
+        BulleteToolStripMenuItem.Size = New Size(243, 30)
+        BulleteToolStripMenuItem.Text = "Bullete"
+        ' 
+        ' YeToolStripMenuItem
+        ' 
+        YeToolStripMenuItem.Name = "YeToolStripMenuItem"
+        YeToolStripMenuItem.Size = New Size(125, 30)
+        YeToolStripMenuItem.Text = "Ye"
+        ' 
+        ' NOToolStripMenuItem
+        ' 
+        NOToolStripMenuItem.Name = "NOToolStripMenuItem"
+        NOToolStripMenuItem.Size = New Size(125, 30)
+        NOToolStripMenuItem.Text = "NO"
+        ' 
+        ' ZoomToolStripMenuItem
+        ' 
+        ZoomToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {ToolStripMenuItem2, ToolStripMenuItem3, ToolStripMenuItem4, ToolStripMenuItem5})
+        ZoomToolStripMenuItem.Name = "ZoomToolStripMenuItem"
+        ZoomToolStripMenuItem.Size = New Size(65, 26)
+        ZoomToolStripMenuItem.Text = "Zoom"
+        ' 
+        ' ToolStripMenuItem2
+        ' 
+        ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        ToolStripMenuItem2.Size = New Size(243, 30)
+        ToolStripMenuItem2.Text = "2"
+        ' 
+        ' ToolStripMenuItem3
+        ' 
+        ToolStripMenuItem3.Name = "ToolStripMenuItem3"
+        ToolStripMenuItem3.Size = New Size(243, 30)
+        ToolStripMenuItem3.Text = "4"
+        ' 
+        ' ToolStripMenuItem4
+        ' 
+        ToolStripMenuItem4.Name = "ToolStripMenuItem4"
+        ToolStripMenuItem4.Size = New Size(243, 30)
+        ToolStripMenuItem4.Text = "6"
+        ' 
+        ' ToolStripMenuItem5
+        ' 
+        ToolStripMenuItem5.Name = "ToolStripMenuItem5"
+        ToolStripMenuItem5.Size = New Size(243, 30)
+        ToolStripMenuItem5.Text = "8"
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.Dock = DockStyle.Fill
+        RichTextBox1.Location = New Point(0, 30)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(800, 420)
+        RichTextBox1.TabIndex = 1
+        RichTextBox1.Text = ""
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.FileName = "OpenFileDialog1"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(9F, 21F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(RichTextBox1)
+        Controls.Add(MenuStrip1)
+        MainMenuStrip = MenuStrip1
+        Name = "Form1"
+        Text = "Form1"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopyToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PasteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ReduToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents UndoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FormatToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FontToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AlignToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LeftToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RightToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CenterToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BulleteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents YeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NOToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ZoomToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem3 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem4 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem5 As ToolStripMenuItem
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents FontDialog1 As FontDialog
+    Friend WithEvents ColorDialog1 As ColorDialog
+End Class
---
+++ Notepad app/Form1.vb
@@ -0,0 +1,94 @@
+﻿Public Class Form1
+
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+
+        RichTextBox1.Clear()
+
+    End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        SaveFileDialog1.ShowDialog()
+        RichTextBox1.SaveFile(SaveFileDialog1.FileName)
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        OpenFileDialog1.ShowDialog()
+        RichTextBox1.LoadFile(OpenFileDialog1.FileName)
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub CutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CutToolStripMenuItem.Click
+        RichTextBox1.Cut()
+    End Sub
+
+    Private Sub CopyToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopyToolStripMenuItem.Click
+        RichTextBox1.Copy()
+    End Sub
+
+    Private Sub PasteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PasteToolStripMenuItem.Click
+        RichTextBox1.Paste()
+    End Sub
+
+    Private Sub ReduToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ReduToolStripMenuItem.Click
+        RichTextBox1.Redo()
+
+    End Sub
+
+    Private Sub UndoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles UndoToolStripMenuItem.Click
+        RichTextBox1.Undo()
+    End Sub
+
+    Private Sub FontToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FontToolStripMenuItem.Click
+        FontDialog1.ShowDialog()
+        RichTextBox1.SelectionFont = FontDialog1.Font
+    End Sub
+
+    Private Sub ColorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ColorToolStripMenuItem.Click
+        ColorDialog1.ShowDialog()
+        RichTextBox1.SelectionColor = ColorDialog1.Color
+    End Sub
+
+    Private Sub AlignToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AlignToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub LeftToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LeftToolStripMenuItem.Click
+        RichTextBox1.SelectionAlignment = HorizontalAlignment.Left
+    End Sub
+
+    Private Sub RightToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RightToolStripMenuItem.Click
+        RichTextBox1.SelectionAlignment = HorizontalAlignment.Right
+    End Sub
+
+    Private Sub CenterToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CenterToolStripMenuItem.Click
+        RichTextBox1.SelectionAlignment = HorizontalAlignment.Center
+    End Sub
+
+    Private Sub YeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles YeToolStripMenuItem.Click
+        RichTextBox1.SelectionBullet = True
+    End Sub
+
+    Private Sub NOToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NOToolStripMenuItem.Click
+        RichTextBox1.SelectionBullet = False
+    End Sub
+
+    Private Sub ToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem2.Click
+        RichTextBox1.ZoomFactor = 2
+    End Sub
+
+    Private Sub ToolStripMenuItem3_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem3.Click
+        RichTextBox1.ZoomFactor = 4
+    End Sub
+
+    Private Sub ToolStripMenuItem4_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem4.Click
+        RichTextBox1.ZoomFactor = 6
+    End Sub
+
+    Private Sub ToolStripMenuItem5_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem5.Click
+        RichTextBox1.ZoomFactor = 8
+    End Sub
+End Class
